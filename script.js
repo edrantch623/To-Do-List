082424d479347248a97d845082e2365d555ad086
edrantch623 <evan.drantch@gmail.com>
2021-07-28 16:37:05 -0500

    Updated code

--- script.js
+++ script.js
@@ -1,55 +1,55 @@
-
-// Create Close button and attach to each task
-let taskList = document.getElementsByTagName("LI");
-let i;
-for (i = 0; i < taskList.length; i++) {
-    let span = document.createElement("SPAN");
-    let txt = document.createTextNode("\u00D7"); // Adds "X" as close button
-    span.className = "close";
-    span.appendChild(txt);
-    taskList[i].appendChild(span);
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
 }
 
-// Clicking close button to hide selected task
-let close = document.getElementsByClassName("close");
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
 for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-        let div = this.parentElement;
-        div.style.display = "none";
-    }
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
 }
 
-// Adds Check when clicking on Task
-let list = document.querySelector('ul');
-list.addEventListener('click', function(ev){
-    if (ev.target.tagName === 'LI') {
-        ev.target.classList.toggle('checked');
-    }
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
 }, false);
 
-// Add new Task when Add button is clicked
-function newTask() {
-    let li = document.createElement("li");
-    let inputValue = document.getElementById("textEntryField").value;
-    let t = document.createTextNode(inputValue);
-    li.appendChild(t);
-    if (inputValue === '') {
-        alert("You must enter a task");
-    } else {
-        document.getElementById("taskList").appendChild(li);
-    }
-    document.getElementById("textEntryField").value = "";
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
 
-    let span = document.createElement("SPAN");
-    let txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
 
-    for (i = 0; i < close.length; i++) {
-        close[i].onclick = function() {
-            let div = this.parentElement;
-            div.style.display = "none";
-        }
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
     }
+  }
 }
